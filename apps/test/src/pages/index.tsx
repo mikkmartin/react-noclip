dde9f28ea35acfa31f46f7cc8f362c4f7248cacb
Mikk Martin <mikk.martin@gmail.com>
2022-12-06 10:20:05 +0200

    motion test

--- apps/test/src/pages/index.tsx
+++ apps/test/src/pages/index.tsx
@@ -1,18 +1,39 @@
 import { useState } from "react";
 import { useNoclip } from "react-noclip";
+import { motion } from "framer-motion";
 
 export default function Docs() {
   const [showHeader, setShowHeader] = useState(true);
+  const [key, setKey] = useState(0);
 
   useNoclip({
     test: () => console.log("test"),
     bob: () => console.log("bob"),
     toggleHeader: () => setShowHeader(!showHeader),
+    restartAnimation: () => setKey(key + 1),
   });
 
   return (
-    <div>
-      {showHeader && <h1>react-noclip Documentation</h1>}
+    <div
+      style={{
+        padding: "10rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: 10,
+        alignItems: "center",
+      }}
+    >
+      {showHeader && (
+        <motion.h1
+          key={key}
+          transition={{ type: "spring", bounce: 200, damping: 20 }}
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+        >
+          react-noclip Documentation
+        </motion.h1>
+      )}
       <input type="text" />
       <button>does nothing</button>
     </div>
