90971b1892db78ea042eb6a0a1d94079fd41de50
Mikk Martin <mikk.martin@gmail.com>
2024-09-19 09:49:38 +0300

    test app fix

--- apps/test/src/pages/_app.tsx
+++ apps/test/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { ThemeProvider } from "next-themes";
-import { GlobalStyles } from '../../components/GlobalStyles'
+import { GlobalStyles } from '../components/GlobalStyles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
