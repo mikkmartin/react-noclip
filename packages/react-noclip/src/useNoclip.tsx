5413b5db97458f424cff1c78918bf6d5dabc3c49
Mikk Martin <mikk.martin@gmail.com>
2024-09-22 09:56:06 +0300

    Update useNoclip.tsx

--- packages/react-noclip/src/useNoclip.tsx
+++ packages/react-noclip/src/useNoclip.tsx
@@ -55,7 +55,10 @@ export function useNoclip(content: Content) {
   }
 
   const onKeyDown = (e: KeyboardEvent) => {
-    if (e.key === "k" && (e.metaKey || e.ctrlKey) && !mountedRef.current) mount();
+    if (e.key === "k" && (e.metaKey || e.ctrlKey) && !mountedRef.current) {
+      e.preventDefault();
+      mount()
+    };
   };
 
   useEffect(() => {
